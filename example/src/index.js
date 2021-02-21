62702ce586167c9327f0994a250a5b48a29d3e7f
sinnerJoe <ianknote@gmail.com>
2021-02-21 15:31:17 +0200

    solution to the exercise

--- example/src/index.js
+++ example/src/index.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { render } from 'react-dom';
-import { useIntl } from 'ebs-intl';
+import { useIntl, generateTranslation, LocaleContext, Locale } from 'ebs-intl';
 
-const App = () => {
-  const t = useIntl();
+const createLocaleState = (lang) => ({
+  lang,
+  t: generateTranslation(lang)
+});
 
-  return <div>{t('greeting')}</div>;
+const Root = () => {
+  const [localeState, setLocaleState] = useState(() => createLocaleState(Locale.English));
+  return (
+    <LocaleContext.Provider value={localeState}>
+      <App onLocaleChange={(lang) => setLocaleState(createLocaleState(lang))} />
+    </LocaleContext.Provider>
+  )
+}
+
+const rollLocale = (locale) => {
+  return {
+    [Locale.English]: Locale.Romanian,
+    [Locale.Romanian]: Locale.Russian,
+    [Locale.Russian]: Locale.English,
+  }[locale]
+}
+
+const App = ({onLocaleChange}) => {
+  const {t, lang} = useIntl();
+
+  return <div>
+    <div>{t('hello', {name: 'Абэма'})}</div>
+    <button onClick={() => onLocaleChange(rollLocale(lang))}>Change language</button>
+  </div>;
 };
 
-render(<App />, document.getElementById('root'));
+render(<Root />, document.getElementById('root'));
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,53 @@
-export const useIntl = () => {
-  const intls = {
-    greeting: 'Hello, World!',
-  };
+import {createContext, useContext} from 'react'
 
-  return (key: string): string => intls[key];
+
+export enum Locale {
+  English = 'en',
+  Romanian = 'ro',
+  Russian = 'ru'
+}
+
+export interface Translate {
+   [translationKey: string]: Record<Locale, string>
+}
+
+export interface TranslateUtils {
+   lang: Locale,
+   t: (key: string, params?: { [key: string]: string }) => string
+}
+
+const intls: Translate = {
+  hello: {
+    en: 'Hello {name}!',
+    ro: 'Salut {name}!',
+    ru: 'Привет {name}!'
+  },
+};
+
+function pickEntries(language: Locale): Record<keyof Translate, string> {
+  const obj = {};
+  for(const token of Object.keys(intls)) {
+    obj[token] = intls[token][language];
+  }
+  return obj;
+}
+
+function translate(entries: Record<string, string>, key: string, params?: Record<string, string>) {
+  const hostToken = entries[key];
+  if(!params) {
+    return hostToken;
+  }
+
+  return Object.entries(params).reduce((acc, [key, value]) => acc.replace(new RegExp(`{${key}}`, 'g'), value), hostToken);
+}
+
+export function generateTranslation(language: Locale) {
+  const entries = pickEntries(language);
+  return (key: string, params?: Record<string, string>) => translate(entries, key, params);
+}
+
+export const LocaleContext = createContext<TranslateUtils>({lang: Locale.English, t: generateTranslation(Locale.English)});
+
+export function useIntl() {
+  return useContext(LocaleContext);
 };
